245ab76bfe2ce168a3a35c8319b7d710ba80c906
Nekoziller <72552479+Nekoziller@users.noreply.github.com>
2022-06-01 06:22:31 +0900

    fix files add SERVER_ID

--- bot.py
+++ bot.py
@@ -22,13 +22,13 @@ class Discord_bot(commands.Bot):
         super().__init__(command_prefix="-", case_insensitive=True, intents=intents)
 
     async def on_ready(self):
-        await self.tree.sync(guild=discord.Object(id=(os.getenv('SERVER_ID'))))
         for cog in INITIAL_EXTENSIONS:
             await bot.load_extension(cog)
         print(f'{bot.user} is Ready')
 
     async def setup_hook(self) -> None:
         self.session = aiohttp.ClientSession()
+        await self.tree.sync(guild=discord.Object(id=(os.getenv('SERVER_ID'))))
         self.bot_app_info = await self.application_info()
         self.owner_id = self.bot_app_info.owner.id
 
